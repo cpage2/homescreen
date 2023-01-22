cd887ae7d9aaf63d3ba7e72b24412d5f696b6257
cpage2 <mcpage25@gmail.com>
2023-01-22 11:31:14 -0600

    repository created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import Moment from 'react-moment';
+import moment from 'moment';
 
+
+
+//tm.jpg
 function App() {
+
+
+  //style for fonts
+  let fontStyle ={
+    fontSize: 20,
+    color: 'white'
+
+  };
+
+
+  // var { currentDate, time } = TimeDate();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //date
+    //time
+    //qoute in transparent rectangle
+   
+    <div >
+     
+      
+       <Moment format='LL'></Moment><><br></br></>
+       <Moment format='LTS'></Moment>
+      
     </div>
+
+
+
   );
+
+
+
+
+  
+
+  function TimeDate() {
+    var date = moment();
+    var currentDate = date.format("LL");
+
+    var time = date.format("LTS");
+    
+    return (
+       <><p style={fontStyle}>{currentDate}</p><p style={fontStyle}>{time}</p></>);
+  }
+
+
+
 }
 
 export default App;
