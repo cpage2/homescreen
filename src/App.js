c3c29a0bceb366cff96f7b050dc8f27525ed05b3
cpage2 <mcpage25@gmail.com>
2023-01-22 13:52:26 -0600

    add styling to qouteBox,credit,dateTime

--- src/App.js
+++ src/App.js
@@ -10,10 +10,20 @@ function App() {
 
   //style for fonts
   let fontStyle ={
-    fontSize: 20,
+    fontSize: 12,
     color: 'white'
 
   };
+
+  let dtStyle ={
+    fontSize: 20,
+    color: 'white'
+  }
+
+
+  let divBG ={
+    backgroundColor: 'rgba(0, 0, 0, 0.4)'
+  }
 
 
   return (
@@ -24,20 +34,20 @@ function App() {
     <>
      
       <div className='dateTime'>
-        <><Moment style={fontStyle} className='date' format='LL' interval={1000} fromNow></Moment>
+        <><Moment style={dtStyle} className='date' format='LL' interval={1000} fromNow></Moment>
           <><br></br></>
-          <Moment style={fontStyle} className='time' format='LT' interval={1000} fromNow></Moment></>
+          <Moment style={dtStyle} className='time' format='LT' interval={1000} fromNow></Moment></>
 
       </div>
       
       
-      <div className='qouteBox'>
-          <p id='qoute'>“I hope that you never cower from your dark chapters, but highlight them as proof of perseverance, endurance, and strength.”<br></br>
+      <div className='qouteBox' style={divBG}>
+          <p id='qoute' style={fontStyle}>“I hope that you never cower from your dark chapters, but highlight them as proof of perseverance, endurance, and strength.”<br></br>
             ― Kierra C.T. Banks</p>
       
       </div>
       
-      <footer><p id='credit'>Photo by <a href="https://unsplash.com/@miyatankun?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Takashi Miyazaki</a> on <a href="https://unsplash.com/photos/4VELvD-vdVg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p></footer>
+      <footer><p id='credit' style={fontStyle}>Photo by <a href="https://unsplash.com/@miyatankun?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Takashi Miyazaki</a> on <a href="https://unsplash.com/photos/4VELvD-vdVg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p></footer>
       </>
 
 
